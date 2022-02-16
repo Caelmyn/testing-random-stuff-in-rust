b114bfac123222f7862c137dfa090dae50d7a1f9
Maxime BAILLY <maxbailly@hotmail.fr>
2022-02-16 17:25:59 +0100

    grid: fix incorrect iter_mut + add more tests

--- crates/grid/src/iteration/cursor.rs
+++ crates/grid/src/iteration/cursor.rs
@@ -30,7 +30,7 @@ impl Cursor {
     #[doc(hidden)]
     #[inline]
     pub(super) fn next(&mut self) {
-        let (x, y) = utils::coord_form_index(self.cursor, self.grid_width);
+        let (x, y) = utils::coords_from_index(self.cursor, self.grid_width);
         if x >= self.area.right {
             self.cursor = utils::index_from_coord(self.area.left, y + 1, self.grid_width);
         } else {
@@ -42,7 +42,7 @@ impl Cursor {
     #[doc(hidden)]
     #[inline]
     pub(super) const fn is_valid(&self) -> bool {
-        let (x, y) = utils::coord_form_index(self.cursor, self.grid_width);
+        let (x, y) = utils::coords_from_index(self.cursor, self.grid_width);
         x <= self.area.right && y <= self.area.bottom
     }
 
@@ -51,5 +51,32 @@ impl Cursor {
     #[inline]
     pub(super) const fn index(&self) -> usize {
         self.cursor
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Area;
+    use super::Cursor;
+
+    const GRID_SIZE: usize = 1000;
+
+    #[test]
+    fn new_cursor() {
+        let area = Area::new(0, 0, 0, 0);
+        let cursor = Cursor::new(GRID_SIZE, area);
+        assert_eq!(cursor.cursor, 0);
+
+        let area = Area::new(0, 1, 0, 0);
+        let cursor = Cursor::new(GRID_SIZE, area);
+        assert_eq!(cursor.cursor, 0);
+
+        let area = Area::new(0, 1, 0, 1);
+        let cursor = Cursor::new(GRID_SIZE, area);
+        assert_eq!(cursor.cursor, 1);
+
+        let area = Area::new(959, 489, 964, 759);
+        let cursor = Cursor::new(GRID_SIZE, area);
+        assert_eq!(cursor.cursor, 959489);
     }
 }
--- crates/grid/tests/test.rs
+++ crates/grid/tests/test.rs
@@ -4,6 +4,30 @@ use grid::{Area, Grid};
 
 /* ---------- */
 
+#[derive(Debug, Default, Clone, Copy)]
+struct Witness {
+    id: usize,
+    status: bool
+}
+
+impl Witness {
+    fn set_status(&mut self) {
+        self.status = true;
+    }
+
+    fn unset_status(&mut self) {
+        self.status = false;
+    }
+}
+
+impl PartialEq for Witness {
+    fn eq(&self, other: &Self) -> bool {
+        self.status == other.status
+    }
+}
+
+/* ---------- */
+
 const fn coord_from_idx(idx: usize, width: usize) -> (usize, usize) {
     (idx % width, idx / width)
 }
@@ -18,6 +42,10 @@ fn test_value<T: Default + PartialEq + Debug>(grid: Grid<T>, val: T, width: usiz
         let val_coord = grid.get(x, y);
         assert_eq!(val_coord, Some(&val));
     }
+}
+
+const fn is_contained_in_area((x, y): (usize, usize), area: Area) -> bool {
+    x >= area.left && x <= area.right && y >= area.top && y <= area.bottom
 }
 
 /* ---------- */
@@ -87,8 +115,14 @@ fn test_iter_mut() {
 
     assert_eq!(grid.iter_mut().count(), 100);
     assert_eq!(grid.iter().sum::<i32>(), 0);
+
     grid.iter_mut().for_each(|val| *val = 10);
     assert_eq!(grid.iter().sum::<i32>(), 1000);
+
+    grid.iter_mut().enumerate().for_each(|(index, val)| *val = index as i32);
+    for index in 0..10*10 {
+        assert_eq!(grid.get_at_index(index), Some(&(index as i32)))
+    }
 }
 
 /* ---------- */
@@ -99,8 +133,56 @@ fn test_iter_mut_over() {
 
     assert_eq!(grid.iter_mut().count(), 100);
     assert_eq!(grid.iter().sum::<i32>(), 0);
-    grid.iter_mut().for_each(|val| *val = 10);
+    grid.iter_over_mut(Area::new(0, 0, 10, 10)).for_each(|val| *val = 10);
     assert_eq!(grid.iter().sum::<i32>(), 1000);
+
+    let mut grid = Grid::new_square_filled(1000, Witness::default());
+    assert_eq!(grid.iter_mut().count(), 1_000_000);
+    assert_eq!(grid.iter_mut().filter(|item| item.status).count(), 0);
+
+    grid.iter_mut().enumerate().for_each(|(index, val)| val.id = index);
+
+    for index in 0..1000*1000 {
+        let wit = grid.get_at_index(index).unwrap();
+        assert!(!wit.status);
+        assert_eq!(wit.id, index)
+    }
+
+    grid.iter_over_mut(Area::new(959, 489, 964, 759)).enumerate().for_each(|(_, val)| val.set_status());
+    for index in 0..1000*1000 {
+        let coords = coord_from_idx(index, 1000);
+        let val = grid.get(coords.0, coords.1).unwrap();
+
+        if is_contained_in_area(coords, Area::new(959, 489, 964, 759)) {
+            assert_eq!(val, &Witness { id: index, status: true });
+        } else {
+            assert_eq!(val, &Witness { id: index, status: false });
+        }
+    }
+
+    grid.iter_mut().enumerate().for_each(|(_, val)| val.unset_status());
+    grid.iter_over_mut(Area::new(0, 0, 0, 999)).enumerate().for_each(|(_, val)| val.set_status());
+    for index in 0..1000*1000 {
+        let coords = coord_from_idx(index, 1000);
+        let val = grid.get(coords.0, coords.1).unwrap();
+
+        if is_contained_in_area(coords, Area::new(0, 0, 0, 999)) {
+            assert_eq!(val, &Witness { id: index, status: true });
+        } else {
+            assert_eq!(val, &Witness { id: index, status: false });
+        }
+    }
+}
+
+/* ---------- */
+
+#[test]
+fn test_area_elements() {
+    assert_eq!(Area::new(0, 0, 0, 0).area(), 1);
+    assert_eq!(Area::new(999, 999, 999, 999).area(), 1);
+    assert_eq!(Area::new(0, 0, 999, 999).area(), 1_000_000);
+    assert_eq!(Area::new(999, 999, 0, 0).area(), 1_000_000);
+    assert_eq!(Area::new(959, 489, 964, 759).area(), 1626);
 }
 
 /* ---------- */

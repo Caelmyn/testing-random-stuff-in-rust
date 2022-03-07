524531f0eb0b94e93553f010bb5cdd4b0754327d
Maxime BAILLY <maxbailly@hotmail.fr>
2022-03-07 20:08:51 +0100

    grid : more benches

--- crates/grid/benches/grid_bench.rs
+++ crates/grid/benches/grid_bench.rs
@@ -18,8 +18,11 @@ pub fn from_areas(c: &mut Criterion) {
     let mut group = c.benchmark_group("from_areas");
 
     group.significance_level(0.1).sample_size(500);
-    group.bench_with_input(BenchmarkId::from_parameter("Grid"), &Area::new(0, 0, 999, 999), |b, area| {
+    group.bench_with_input(BenchmarkId::from_parameter("Iter Over"), &Area::new(0, 0, 999, 999), |b, area| {
         b.iter(|| grid.iter_over_mut(*area).count());
+    });
+    group.bench_with_input(BenchmarkId::from_parameter("Iter Mut"), &Area::new(0, 0, 999, 999), |b, _| {
+        b.iter(|| grid.iter_mut().count());
     });
     group.bench_with_input(BenchmarkId::from_parameter("Manually"), &Area::new(0, 0, 999, 999), |b, area| {
         b.iter(|| (manually(&grid, criterion::black_box(area.top), criterion::black_box(area.left), criterion::black_box(area.bottom), criterion::black_box(area.right))));
